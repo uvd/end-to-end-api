6bca16c4dd744aba02dd2762c9c2b3f232a7fe9f
Alex Longshaw <alexlongshaw@gmail.com>
2016-07-29 14:57:30 +0100

    Add play count incrementer

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as firebase from 'firebase';
+import * as socketIo from 'socket.io';
+import * as http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const firebaseApp = firebase.initializeApp({
     apiKey: process.env.E2E_FIREBASE_API_KEY,
     authDomain: "end-to-end-fm.firebaseapp.com",
@@ -23,10 +28,30 @@ app.use((req, res, next) => {
 app.get('/episodes', (req, res, next) => {
 
     firebase.database().ref('episodes').once('value').then(function(snapshot) {
-        let episodes = snapshot.val();
+        let episodes: e2e.IEpisode = snapshot.val();
         res.send(episodes);
     });
+
+});
+
+
+
+io.on('connection', function(socket) {
+
+    socket.on('episode:played', (data) => {
+        let id = data.id || JSON.parse(data).id;
+        const playCountRef = firebase.database().ref(`episodes/${id}/play_count`);
+
+        playCountRef.once('value').then((snapshot) => {
+            let playCount: number = snapshot.val();
+            playCount++;
+            playCountRef.set(playCount);
+        });
+
+    });
+
 });
 
 
 app.listen(process.env.PORT || 8087);
+io.listen(3000);
--- lib/typings/end-to-end.d.ts
+++ lib/typings/end-to-end.d.ts
@@ -1,5 +1,20 @@
 
-declare module "e2e" {
+declare module e2e {
+
+    interface IEpisode {
+        title: string;
+        url: string;
+        duration: {
+            hours: number;
+            minutes: number;
+            seconds: number;
+        }
+        date: number;
+        speakers: string[];
+        description: string;
+        id: number;
+        play_count: number;
+    }
 
 }
 
