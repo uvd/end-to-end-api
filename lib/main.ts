fd9cc56610d3467035aac6cf4d2313faa46daad3
Alex Longshaw <alexlongshaw@gmail.com>
2016-07-29 10:50:07 +0100

    Added GET episodes path and connected to firebase

--- lib/main.ts
+++ lib/main.ts
@@ -1,13 +1,31 @@
 import * as express from 'express';
-import bodyParser = require('body-parser');
+import * as bodyParser from 'body-parser';
+import * as firebase from 'firebase';
 
 const app = express();
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.E2E_FIREBASE_API_KEY,
+    authDomain: "end-to-end-fm.firebaseapp.com",
+    databaseURL: "https://end-to-end-fm.firebaseio.com",
+    storageBucket: "end-to-end-fm.appspot.com",
+});
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+
+
+app.get('/episodes', (req, res, next) => {
+
+    firebase.database().ref('episodes').once('value').then(function(snapshot) {
+        let episodes = snapshot.val();
+        res.send(episodes);
+    });
 });
 
 
